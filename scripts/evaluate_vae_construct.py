3a83f18c88ea012b10e09478d0620c487c16d588
qq <qianq9720@163.com>
2023-05-10 11:15:38 +0800

    vae_construct value

--- scripts/evaluate_vae_construct.py
+++ scripts/evaluate_vae_construct.py
@@ -1,11 +1,17 @@
+from tqdm import trange
+from PIL import Image
 from pathlib import Path
 import torch
 from torchvision import utils
 import math
 from medical_diffusion.models.embedders.latent_embedders import VAE
 from medical_diffusion.data.datasets import CheXpert_2_Dataset_test
-from PIL import Image
-from torchvision import transforms
+from torch.utils.data.dataloader import DataLoader
+import torchvision.transforms.functional as tF
+from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity as LPIPS
+from torchmetrics.functional import (
+    multiscale_structural_similarity_index_measure as mmssim,
+)
 
 
 def rgb2gray(img):
@@ -26,7 +32,7 @@ def concat_pair_images(source_img, target_img, width, height, save_path):
     concat_image.save(save_path)
 
 
-if __name__ == "__main__":
+def evaluate(save_img=False):
     path_out = Path.cwd() / "results/CheXpert/samples_vae_00"
     path_out.mkdir(parents=True, exist_ok=True)
 
@@ -58,67 +64,78 @@ if __name__ == "__main__":
     )
 
     model.load_pretrained(
-        Path.cwd() / "runs/2023_05_07_170136/epoch=999-step=62000.ckpt", strict=True
+        Path.cwd() / "runs/2023_05_07_170136/epoch=999-step=62000.ckpt",
+        strict=True,
     )
-    # model.eval()
+    model.eval()
     sample_nums = 10
 
+    # 出图
     results = []
     for i in range(sample_nums):
         with torch.no_grad():
             data = dataset[i]["source"]
-            # the conv layer of model needs dim0=8 &4D Tensor input(why 8?)
             input = data.unsqueeze(0)
-            for _ in range(7):
-                input = torch.cat((input, data.unsqueeze(0)), dim=0)
             out, _, emb_loss = model.forward(input)
-            out = (out + 1) / 2
-            out = out.clamp(0, 1)
+            # out = (out + 1) / 2
+            # out = out.clamp(0, 1)
             results.append(out[0])
 
+    calc_lpips = LPIPS().to(device)
+    mmssim_list, mse_list = [], []
     for i in range(len(results)):
-        result = results[i]
-        result = (result + 1) / 2
-        result = result.clamp(0, 1)
-        source = dataset[i]["source"]
+        result = normalize(results[i])
+        source = normalize(dataset[i]["source"])
+        # source = (source + 1) / 2
+        # source = source.clamp(0, 1)
         nrow = int(math.sqrt(result.shape[0]))
 
-        result_path = path_out / f"test_vae_{i}.png"
-        source_path = path_out / f"test_vae_source_{i}.png"
-        save_path = path_out / f"test_vae_paired_{i}.png"
+        # print(f"source: max={torch.max(source)}, min={torch.min(source)}, avg={torch.mean(source)}")
+        # print(f"result: max={torch.max(result)}, min={torch.min(result)}, avg={torch.mean(result)}")
 
-        utils.save_image(
-            result,
-            result_path,
-            nrow=nrow,
-            normalize=True,
-            scale_each=True,
-        )  # For 2D images: [B, C, H, W]
-        utils.save_image(
-            dataset[i]["source"],
-            source_path,
-            nrow=nrow,
-            normalize=True,
-            scale_each=True,
-        )  # For 2D images: [B, C, H, W]
-        concat_pair_images(
-            Image.open(source_path),
-            Image.open(result_path),
-            256,
-            256,
-            save_path,
-        )
-        
-        
+        # evaluation
+        if save_img:
+            result_path = path_out / f"test_vae_{i}.png"
+            source_path = path_out / f"test_vae_source_{i}.png"
+            save_path = path_out / f"test_vae_paired_{i}.png"
+
+            utils.save_image(
+                result,
+                result_path,
+                nrow=nrow,
+                normalize=True,
+                scale_each=True,
+            )  # For 2D images: [B, C, H, W]
+            utils.save_image(
+                dataset[i]["source"],
+                source_path,
+                nrow=nrow,
+                normalize=True,
+                scale_each=True,
+            )  # For 2D images: [B, C, H, W]
+            concat_pair_images(
+                Image.open(source_path),
+                Image.open(result_path),
+                256,
+                256,
+                save_path,
+            )
+
+        source_extend = source.unsqueeze(0)
+        result_extend = result.unsqueeze(0)
+
+        calc_lpips.update(source_extend.to(device), result_extend.to(device))
+        mmssim_list.append(mmssim(source_extend, result_extend, normalize="relu"))
+        mse_list.append(torch.mean(torch.square(source - result)))
+
+    mmssim_list = torch.stack(mmssim_list)
+    mse_list = torch.stack(mse_list)
+    lpips = 1 - calc_lpips.compute()
+
+    print(f"LPIPS Score: {lpips}")
+    print(f"MS-SSIM: {torch.mean(mmssim_list)} ± {torch.std(mmssim_list)}")
+    print(f"MSE: {torch.mean(mse_list)} ± {torch.std(mse_list)}")
 
 
-        
-
-    # results = (results+1)/2
-    # results = results.clamp(0, 1)
-    # utils.save_image(results, path_out/f'test_{cond}.png', nrow=int(math.sqrt(results.shape[0])), normalize=True, scale_each=True) # For 2D images: [B, C, H, W]
-    # print(results)
-
-    # diff = torch.abs(normalize(rgb2gray(images[1]))-normalize(rgb2gray(images[0]))) # [0,1] -> [0, 1]
-    # # diff = torch.abs(images[1]-images[0])
-    # utils.save_image(diff, path_out/'diff.png', nrow=int(math.sqrt(results.shape[0])), normalize=True, scale_each=True) # For 2D images: [B, C, H, W]
+if __name__ == "__main__":
+    evaluate(save_img=True)
